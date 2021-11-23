d5151907b6f5e664c0dda35cd4445ae8d0b1217a
Mikel Azqueta <65721391+mikelazquetax@users.noreply.github.com>
2021-11-23 16:41:56 +0100

    Iteration#3

---
+++ upgrade-basic-algorithms/iteracion3.js
@@ -0,0 +1,24 @@
+//1.1 Multiplica 10 por 5 y muestra el resultado mediante alert.
+
+var multiplicacion = 10 * 5;
+alert(multiplicacion);
+
+//1.2 Divide 10 por 2 y muestra el resultado en un alert.
+
+var division = 10 / 2;
+alert(division);
+
+//1.3 Muestra mediante un alert el resto de dividir 15 por 9.
+
+var resto = 15 % 9;
+
+//1.4 Usa el correcto operador de asignación que resultará en x = 15, teniendo dos variables y = 10 y z = 5.
+
+var y = 10;
+var z = 5;
+
+var x = y + z;
+
+//1.5 Usa el correcto operador de asignación que resultará en x = 50, teniendo dos variables y = 10 y z = 5.
+
+x = y * z;
